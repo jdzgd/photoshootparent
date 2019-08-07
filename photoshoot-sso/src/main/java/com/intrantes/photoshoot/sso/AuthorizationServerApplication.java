7111c5f48463e006ec919dec15ee639f28da8e2f
J <jdzgd@qq.com>
2019-08-07 14:28:32 +0800

    photoshoot-sso模块

---
+++ photoshoot-sso/src/main/java/com/intrantes/photoshoot/sso/AuthorizationServerApplication.java
@@ -0,0 +1,24 @@
+package com.intrantes.photoshoot.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+
+/**
+ * sso授权服务
+ * <br/>
+ *
+ * @author jiangdongzhao
+ * @date 2019/8/7
+ */
+@SpringBootApplication
+@EnableResourceServer
+public class AuthorizationServerApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+}
+
+
